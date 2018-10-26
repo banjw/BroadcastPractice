1e409a185ab5fa4e4745f59041643e593b166f2f
ban <jiawei.ban@qq.com>
2018-10-26 17:53:11 +0800

    修改代码，记住密码功能

--- app/src/main/java/com/example/xk/broadcastpractice/LoginActivity.java
+++ app/src/main/java/com/example/xk/broadcastpractice/LoginActivity.java
@@ -1,14 +1,20 @@
 package com.example.xk.broadcastpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
+
+    private SharedPreferences pref;
+
+    private SharedPreferences.Editor editor;
 
     private EditText accountEdit;
 
@@ -16,11 +22,16 @@ public class LoginActivity extends BaseActivity {
 
     private Button login;
 
+    private CheckBox rememberAccount;
+
+    private CheckBox rememberPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+//        pref = Preference
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
